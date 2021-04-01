b971c4f53fe06872f51796ba93e22b285db5855e
SamiKamal <samixgames@hotmail.com>
2021-04-01 16:04:30 +0300

    small adjusments

---
+++ dist/js/index.js
@@ -0,0 +1,20 @@
+const addGlitchEffect = e => {
+    if (e.target.closest('.cards__box')){
+        e.target.closest('.cards__box--heading')?.classList.add('heading--effect')
+        e.target.closest('.cards__box--heading')?.setAttribute("data-text", e.target.closest('.cards__box--heading').textContent);
+    }
+}
+
+document.querySelectorAll('.cards__box').forEach(el => {
+    el.addEventListener('mouseover', addGlitchEffect)
+})
+
+document.querySelectorAll('.cards__box').forEach(el => {
+    el.addEventListener('mouseleave', e =>{
+        document.querySelectorAll('.cards__box--heading').forEach(el => {
+            el.classList.remove('heading--effect')
+            el.removeAttribute("data-text");
+        })
+
+    })
+})
